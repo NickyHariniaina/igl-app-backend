effa2a0cecfa10b6f6ced6988ebdf3eb9bbc15c1
NickyHariniaina <NickyHarini1na@gmail.com>
2025-05-07 12:29:14 +0300

    Add: neon database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,18 +3,12 @@ import { config } from "dotenv";
 
 config();
 
-const { USER, PASSWORD, HOST, PORT, DATABASE_PORT, DATABASE } = process.env;
-
 // I've used my own supabase database.
 // Feel free to change it, use your own server. 
 // DON'T CHANGE THE NAME OF THE CONST !!!
 
 export const sql = new Pool(
     {
-        user: USER,
-        password: PASSWORD,
-        host: HOST,
-        port: DATABASE_PORT,
-        database: DATABASE, 
+        connectionString: process.env.DATABASE_URL,
     }    
 )
