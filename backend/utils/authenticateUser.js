96516b276348c5fdbe85631a0b10b2794cc68400
NickyHariniaina <nickyharini1na@gmail.com>
2025-06-17 13:04:33 +0300

    fix: token generator

--- backend/utils/authenticateUser.js
+++ backend/utils/authenticateUser.js
@@ -5,21 +5,21 @@ config();
 
 
 export const authenticateUser = (req, res, next) => {
-    const token = req.header('Authorization')?.replace("Bearer ", "");
-    if (!token) {
-        res.status(404).json({
-            message: "Please create an account or log."
-        })
-    } else {
-        try {
-            const decoded = jwt.verify(token, process.env.SECRET_KEY);
-            // Data a utiliser au lieu d'utiliser "req" lors des requettes.
-            req.user = decoded;
-            console.log(req.user);
-            next();
-        } catch (error) {
-            console.error(error);
-            res.send("invalid token.");
-        }
+  const token = req.header('Authorization')?.replace("Bearer ", "");
+  if (!token) {
+    res.status(404).json({
+      message: "Please create an account or log."
+    })
+  } else {
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired." });
+      }
+      return res.status(401).json({ message: "Invalid token." });
     }
+  }
 }
