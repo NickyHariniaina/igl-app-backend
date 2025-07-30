f1545c2d162384fa0e66dce39584961922bac1c5
NickyHariniaina <nickyharini1na@gmail.com>
2025-07-30 10:59:56 +0300

    fix: database connexion

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,16 +4,18 @@ import { Pool } from "pg";
 config();
 
 // I've used my own supabase database.
-// Feel free to change it, use your own server. 
+// Feel free to change it, use your own server.
 // DON'T CHANGE THE NAME OF THE CONST !!!
 
-export const sql = new Pool(
-  {
-    connectionString: process.env.DATABASE_URL,
-    // host: process.env.HOST,
-    // database: process.env.DATABASE,
-    // port: process.env.DATABASE_PORT,
-    // password: process.env.PASSWORD,
-    // user: process.env.USER
-  }
-)
+export const sql =
+  process.env.MODE == "DEV"
+    ? new Pool({
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        port: process.env.DATABASE_PORT,
+        password: process.env.PASSWORD,
+        user: process.env.USER,
+      })
+    : new Pool({
+        connectionString: process.env.DATABASE_URL,
+      });
